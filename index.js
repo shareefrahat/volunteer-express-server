a4ab6ca07aea375570b45f5b3b538f8458ede164
ShareefRahat <imr4h47@gmail.com>
2022-04-29 12:07:03 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv = require("dotenv").config();
 
 const app = express();
@@ -11,6 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 //------------MongoDB-----------------\\
+
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.6olzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("Database Connected: MongoDB");
+  client.close();
+});
 
 //------------Root API-----------------\\
 
